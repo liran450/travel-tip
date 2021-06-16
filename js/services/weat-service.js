83375446a001a13976b99c49ef1b428019b159be
Sahar Davidyan <Davidyan7@gmail.com>
2021-06-16 18:32:11 +0300

    update weather

--- js/services/weat-service.js
+++ js/services/weat-service.js
@@ -13,16 +13,13 @@ const API_KEY = '5cbb966e302dbebcf80f2eb4fa8eb883';
 
 
 
-
-getWeather(lat, lng)
-    .then(console.log)
-    .catch(console.log)
-
 function getWeather(lat, lng) {
-    const lat = locs[0].lat.toFixed(1)
-    const lng = locs[0].lng.toFixed(1)
+    lat = lat.toFixed(1)
+    lng = lng.toFixed(1)
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
     console.log(url);
     return axios.get(url)
         .then(res => res.data)
+        .then(data => data.main.temp)
+        .then(temp => (temp - 273.15).toFixed(0))
 }
